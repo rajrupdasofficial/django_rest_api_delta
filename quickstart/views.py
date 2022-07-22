50eb2cd08cc8d702b907cad771858bf45a3801fa
RajrupDasid <rajrupdasid@outlook.com>
2022-07-22 21:37:45 +0530

    views file

---
+++ quickstart/views.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
