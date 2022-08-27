ba15f400bbf41b4b30db6fa9219252854138700a
RajrupDasid <rajrupdasid@outlook.com>
2022-08-27 13:21:35 +0530

    test cases done but optimiztion needs

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Snippet
+from django.http import HttpResponse
 # Create your tests here.
+
+class SnippetTestCase(TestCase):
+    def setUp(self):
+        Snippet.objects.create(title="new snippet",code="html5 code <b>this is bold</b>",language="HTML5")
+        Snippet.objects.create(title="new_snippet_for_rust",code="fn main(){ prinln ('hello')}",language="Rust")
+    def test_for_check(self):
+        now = Snippet.objects.get(title="new_snippet_for_rust")
+        #self.assertEqual(now.code(),'the rust code says"hello world"')
+        self.assertEqual(now.title,"new_snippet_for_rust")
