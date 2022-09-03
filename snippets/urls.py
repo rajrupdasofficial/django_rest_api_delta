1be8862da2d31ec2e70155769523c2f016e4bd6a
RajrupDasid <rajrupdasid@outlook.com>
2022-09-03 12:26:17 +0530

    url function changed to url class

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from  .views import snippet_list,snippet_detail
-
+#from  .views import snippet_list,snippet_detail
+from snippets import views
 urlpatterns = [
-        path('snippets/',snippet_list),
-        path('snippets/<int:pk>',snippet_detail)
+        #path('snippets/',snippet_list),
+        #path('snippets/<int:pk>',snippet_detail)
+        path('snippets/', views.SnippetList.as_view()),
+        path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 
 ]
 
